3f08b153411375fe3de7ef7ba575b3c4c7afae2f
Sara Martins <sariinha.martins@gmail.com>
2021-01-31 00:56:25 -0300

    change bg

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
-  margin: auto 10%;
+  margin: auto 4%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
